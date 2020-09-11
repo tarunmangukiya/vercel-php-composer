cf55281707d589ffa9cf6da9157dafdac2d7dde4
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-09-11 16:48:04 +0530

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
 
-throw new RuntimeException('Hello Tracy!');
+throw new RuntimeException('Hello Tarun!');
